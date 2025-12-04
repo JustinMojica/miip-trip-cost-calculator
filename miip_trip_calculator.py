cfd294eb6cbdd1c4fbd3d25b59ec1d8f289cae5f
JustinMojica <justin.mojica@mosaic-international.com>
2025-12-04 09:57:34 -0500

    Update miip_trip_calculator.py

--- miip_trip_calculator.py
+++ miip_trip_calculator.py
@@ -24,11 +24,13 @@ def get_amadeus_client():
     """Initialize Amadeus only if secrets are set."""
     try:
         return Client(
-            client_id=st.secrets["4iGEIhJyBuLwpkk3YV6WUvoQNL0fMHWG"],
-            client_secret=st.secrets["xaE0EZasXwY6KNBD"]
+            client_id=st.secrets["AMADEUS_CLIENT_ID"],
+            client_secret=st.secrets["AMADEUS_CLIENT_SECRET"],
+            hostname="production",  # 👈 use live data
         )
     except Exception:
         return None
+
 
 
 amadeus = get_amadeus_client()
@@ -203,5 +205,6 @@ if st.button("💰 Calculate Trip Cost"):
     st.write(f"Per Diem Total: **${per_diem_total}**")
 
     st.success(f"### **Grand Total: ${total_cost:,.2f}**")
+
 
 
